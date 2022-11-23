4204308a815719b289efa4e033a2a24080396876
Oskreduard <oscar.avila.ing@gmail.com>
2022-11-23 07:04:49 -0500

    Vista HTML funcion crear y actualizar

--- src/app/pages/estudiantes/crear/crear.component.ts
+++ src/app/pages/estudiantes/crear/crear.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Estudiante } from '../../../modelos/estudiante.model';
+import { EstudiantesService } from '../../../servicios/estudiante.service';
 
 @Component({
   selector: 'ngx-crear',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearComponent implements OnInit {
 
-  constructor() { }
+  modoCreacion: boolean = true;
+  id_estudiante: string = "";
+  intentoEnvio: boolean = false;
+  elEstudiante: Estudiante = {
+    cedula: "",
+    nombre: "",
+    apellido: ""
+  }
+  constructor(private miServicioEstudiantes: EstudiantesService,
+              private rutaActiva: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    if (this.rutaActiva.snapshot.params.id_estudiante) {
+      this.modoCreacion = false;
+      this.id_estudiante = this.rutaActiva.snapshot.params.id_estudiante;
+      this.getEstudiante(this.id_estudiante)
+    } else {
+      this.modoCreacion = true;
+    }
   }
-
+  getEstudiante(id: string) {
+    this.miServicioEstudiantes.getEstudiante(id).
+      subscribe(data => {
+        this.elEstudiante = data;
+      });
+  }
+  agregar(): void {
+    if (this.validarDatosCompletos()) {
+      this.intentoEnvio = true;
+      this.miServicioEstudiantes.crear(this.elEstudiante).
+        subscribe(data => {
+          Swal.fire(
+            'Creado',
+            'El estudiante ha sido creado correctamente',
+            'success'
+          )
+          this.router.navigate(["pages/estudiantes/listar"]);
+        });
+    }
+  }
+  editar(): void {
+    if (this.validarDatosCompletos()) {
+      this.miServicioEstudiantes.editar(this.elEstudiante._id, this.elEstudiante).
+        subscribe(data => {
+          Swal.fire(
+          'Actualizado',
+          'El estudiante ha sido actualizado correctamente',
+          'success'
+          )
+        this.router.navigate(["pages/estudiantes/listar"]);
+        });
+    }
+  }
+  validarDatosCompletos():boolean{
+    this.intentoEnvio=true;
+    if(this.elEstudiante.cedula=="" || this.elEstudiante.nombre=="" || this.elEstudiante.apellido==""){
+      return false;
+    }else{
+      return true;
+    }
+  }
 }
